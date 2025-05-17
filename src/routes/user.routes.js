c4a15f73fbb30181ef73919ae7ce02a7f097fb30
Gourav Shintre <gouravshintre02.com>
2025-05-17 22:42:05 +0530

    added routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,41 +1,58 @@
 import { Router } from "express";
-import { loginUser, logoutuser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js";
+import {
+  changeCurrentpassword,
+  getCurrentUser,
+  getWatchHistory,
+  loginUser,
+  logoutuser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
+const router = Router();
 
-const router= Router()
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser,
+);
 
-router.route('/register').post(
-  upload.fields(
-    [
-      {
-        name : "avatar",
-        maxCount : 1
-      },
-      {
-        name : "coverImage",
-        maxCount : 1
-      }
-    ]
-  ),
-  registerUser)
+router.route("/login").post(loginUser);
 
+router.route("/logout").post(isLoggedIn, logoutuser);
 
-router.route('/login').post(loginUser)
+router.route("/refresh-token").post(refreshAccessToken);
 
+router.route("/change-password").post(isLoggedIn, changeCurrentpassword);
 
-router.route('/logout').post(
-  isLoggedIn
-  ,logoutuser)
+router.route("/current-user").get(isLoggedIn, getCurrentUser);
 
-router.route('/refresh-token').post(refreshAccessToken)
+router.route("/update-account-details").patch(updateAccountDetails);
 
+router
+  .route("/update-avatar")
+  .patch(isLoggedIn, upload.single("avatar"), updateAvatar);
 
+router
+  .route("/update-coverImage")
+  .patch(isLoggedIn, upload.single("coverImage"), updateCoverImage);
 
+///c/:username used to get current user details
+router.route("/c/:username").get(isLoggedIn, getCurrentUser);
 
-
-
-
+router.route("/watch-history").get(isLoggedIn, getWatchHistory);
 
 export default router;
