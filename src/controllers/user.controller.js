6f7fcee4db96da553b3d57ba801bc53f92f1de5e
gouravs <gourav.shintre@chitaledairy.com>
2025-05-18 17:17:19 +0530

    added res for channel

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -439,6 +439,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
             else: false,
           },
         },
+        
       },
     },
     {
@@ -453,6 +454,15 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
       },
     },
   ]);
+  if (!channel?.length) {
+    throw new ApiError(404, "channel does not exists")
+}
+
+return res
+.status(200)
+.json(
+    new ApiResponse(200, channel[0], "User channel fetched successfully")
+)
 });
 
 const getWatchHistory = asyncHandler (async (req,res)=>{
